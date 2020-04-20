43bc0d3ff2538fe66f279ddcb8ddfa8497351dd1
Gabriel Saillard <gabriel@saillard.dev>
2020-04-20 14:42:03 +0200

    main loop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+
+// Import scripts
+
+let i = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+	function main() {
+		let next = window.requestAnimationFrame(main);
+
+		document.querySelector('h1').innerText = `${i}`;
+		i++;
+
+		if (i > 1000) {
+			window.cancelAnimationFrame(next);
+		}
+	}
+
+	main();
+});
