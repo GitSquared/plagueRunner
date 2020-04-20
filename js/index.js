5391983bc0076f2239efaa1ae840912f7481b219
Gabriel Saillard <gabriel@saillard.dev>
2020-04-20 22:27:09 +0200

    Hitbox

--- js/index.js
+++ js/index.js
@@ -1,39 +1,118 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
 	function main() {
 		let next = window.requestAnimationFrame(main);
 
+		const playerSize = 40;
+		const speed = 5;
+		const halfSpeed = 3;
+
+		let newx = x;
+		let newy = y;
+		let direction = '';
+
 		if (keyboard.keys.up) {
-			y = y + speed;
+			newy = y - speed;
+			direction = 'n';
 		}
 		if (keyboard.keys.right) {
-			x = x - speed;
+			newx = x + speed;
+			direction = 'e';
 		}
 		if (keyboard.keys.down) {
-			y = y - speed;
+			newy = y + speed;
+			direction = 's';
 		}
 		if (keyboard.keys.left) {
-			x = x + speed;
+			newx = x - speed;
+			direction = 'w';
+		}
+
+		if (keyboard.keys.right && keyboard.keys.up) {
+			newx = x + halfSpeed;
+			newy = y - halfSpeed;
+			direction = 'ne';
+		}
+		if ( keyboard.keys.right && keyboard.keys.down) {
+			newx = x + halfSpeed;
+			newy = y + halfSpeed;
+			direction = 'se';
+		}
+		if (keyboard.keys.left && keyboard.keys.down) {
+			newx = x - halfSpeed;
+			newy = y + halfSpeed;
+			direction = 'sw';
+		}
+		if (keyboard.keys.left && keyboard.keys.up) {
+			newx = x - halfSpeed;
+			newy = y - halfSpeed;
+			direction = 'nw';
+		}
+
+		if (x !== newx || y !== newy) {
+			let testx = newx - playerSize / 2;
+			let testy = newy - playerSize / 2;
+
+			let odd = false;
+			let abandon = false;
+			while (!map.canGo(testx, testy, playerSize, playerSize) && !abandon) {
+				let speedx = newx - x;
+				let speedy = newy - y;
+				console.warn(speedx, speedy);
+
+				if (speedx === 0 && speedy === 0) {
+					abandon = true;
+					break;
+				}
+
+				if (odd && speedx !== 0) {
+					odd = false;
+					if (speedx > 0) {
+						speedx--;
+					} else {
+						speedx++;
+					}
+				} else if (speedy !== 0) {
+					odd = true;
+					if (speedy > 0) {
+						speedy--;
+					} else {
+						speedy++;
+					}
+				} else {
+					if (speedx > 0) {
+						speedx--;
+					} else {
+						speedx++;
+					}
+				}
+
+				newx = x + speedx;
+				newy = y + speedy;
+				testx = newx - playerSize / 2;
+				testy = newy - playerSize / 2;
+			}
+
+			x = newx;
+			y = newy;
 		}
 
 		map.goTo(x, y);
-
-		// if (i > 1000) {
-		//    window.cancelAnimationFrame(next);
-		// }
 	}
 
 	document.querySelector('.loading-screen').classList.add('hidden');
 	const map = new GameMap(
 		document.querySelector('.map'),
 		'assets/images/background.png',
-		[3750, 2710]
+		[3750, 2710],
+		document.querySelector('.hitbox-canvas'),
+		document.querySelector('.hitbox-img')
 	);
 	map.goTo(0, 0);
 
-	let x = 0,
-		y = 0;
+	window.currentMap = map;
 
-	const speed = 5;
+	let x = 0;
+	let y = 0;
 
 	const keyboard = new KeyboardListener();
 
--- js/map.js
+++ js/map.js
@@ -1,8 +1,30 @@
 class GameMap {
-	constructor(mapComponent, textureUrl, textureSize) {
+	constructor(mapComponent, textureUrl, textureSize, hitboxCanvas, hitboxImg) {
 		this.element = mapComponent;
 		this.element.style.background = `center / ${textureSize[0]}px ${textureSize[1]}px url("${textureUrl}") no-repeat`;
 		this.textureSize = textureSize;
+
+		this.hitbox = hitboxCanvas;
+		this.hitbox.width = textureSize[0];
+		this.hitbox.height = textureSize[1];
+
+		this.hitbox.getContext('2d').drawImage(hitboxImg, 0, 0, textureSize[0], textureSize[1]);
+	}
+
+	canGo(x, y, width = 1, height = 1) {
+		console.log('test', x, y, width, height);
+		x = (this.textureSize[0] / 2) + x;
+		y = (this.textureSize[1] / 2) + y;
+		const pixels = this.hitbox.getContext('2d').getImageData(x, y, width, height).data;
+
+		let black = false;
+		for (const pixel of pixels) {
+			if (pixel === 0) {
+				black = true;
+				break;
+			}
+		}
+		return (!black);
 	}
 
 	goTo(x, y) {
@@ -15,8 +37,8 @@ class GameMap {
 		];
 
 		const position = [
-			center[0] + x,
-			center[1] + y
+			center[0] - x,
+			center[1] - y
 		];
 
 		this.element.style.backgroundPosition = `${position[0]}px ${position[1]}px`;
